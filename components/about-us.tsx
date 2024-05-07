b3069d3a5e24cb4db0f000e371ec93159dbf3b02
maungshine <shinekoko1276@gmail.com>
2024-05-07 23:56:57 +0630

    escape singel quotes

--- components/about-us.tsx
+++ components/about-us.tsx
@@ -7,7 +7,7 @@ function AboutUs() {
             <p className='indent-8 px-4'>
                 Welcome to RentX, where finding your dream home is our top priority. At RentX, we understand that the search for the perfect home can be overwhelming and time-consuming. That&apos;s why we&apos;re dedicated to providing you with a seamless and hassle-free experience, making the journey to your dream home an enjoyable one.
 
-                Our mission is simple: to empower you in your quest for the ideal living space. Whether you&apos;re a prospective tenant searching for the perfect rental property or a homeowner looking to list your property for rent, we're here to guide you every step of the way.
+                Our mission is simple: to empower you in your quest for the ideal living space. Whether you&apos;re a prospective tenant searching for the perfect rental property or a homeowner looking to list your property for rent, we&apos;re here to guide you every step of the way.
 
 
             </p>
