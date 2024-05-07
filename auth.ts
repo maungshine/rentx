baf3394f1d8848acdb630617d6468b815638658f
maungshine <shinekoko1276@gmail.com>
2024-05-07 11:01:26 +0630

    log account and profile

--- auth.ts
+++ auth.ts
@@ -63,6 +63,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             }
             //this will run when sign in flag is triggered
             if (account?.provider === 'google') {
+                console.log(account);
+                console.log(profile);
                 token['provider'] = account.provider;
             } else {
                 token.provider = 'credentials';
