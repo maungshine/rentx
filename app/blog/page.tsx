ea20c174b7b195a10f9c962d07e879f67011e203
maungshine <shinekoko1276@gmail.com>
2024-05-14 11:08:55 +0630

    fix typo in blog coming soon

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function BlogPage() {
     return (
         <div className='flex items-center justify-center h-screen'>
-            <p className='text-4xl font-blod'>Comming Soon</p>
+            <p className='text-4xl font-blod'>Coming Soon</p>
         </div>
     )
 }
